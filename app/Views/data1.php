7c1987b70497df8013f3b242d1f20dba56d4e315
sukipri <sukipri9985@gmail.com>
2025-08-21 23:09:56 +0700

    210825

--- app/Views/data1.php
+++ app/Views/data1.php
@@ -5,13 +5,29 @@
 <?php
    $db = db_connect();
     $query = $db->query('select * from nat_dokter order by dokter_nama_01 asc'); 
+echo"<table class='table table-bordered table-striped table-sm'>";
+echo"<tr class='table-dark'>";
 
+    echo "<td>Nik</td>";
+    echo "<td>SS ID</td>";
+    echo "<td>Nama</td>";
+    echo "<td>-</td>";
+    echo"</tr>";
 foreach ($query->getResult('array') as $row) {
-    echo $row['dokter_nik_01'];
-    echo $row['dokter_idss_01'];
-    echo $row['dokter_nama_01'];
-    echo"<br>";
+    echo"<tr>";
+
+    echo "<td>".$row['dokter_nik_01']."</td>";
+    echo "<td>".$row['dokter_idss_01']."</td>";
+    echo "<td>".$row['dokter_nama_01']."</td>";
+    echo"<td>";
+    $query2 = $db->query("select idmain_rjkj1 FROM  nat_rjkj1 WHERE rjkj1_nikdokter_01='$row[dokter_nik_01]'");
+    foreach($query2_f = $query2->getResult('array') as $row2){
+      echo $row2['idmain_rjkj1'];
+    }
+    echo"</td>";
+    echo"</tr>";
 }
+echo"</table>";
 ?>
 
 </div>
